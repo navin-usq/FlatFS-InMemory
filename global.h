a878797035187294ca3b4f2cae9cb1f200e56d6a
navin-usq <37042506+navin-usq@users.noreply.github.com>
2018-11-04 15:43:47 +1000

    Update global.h

--- global.h
+++ global.h
@@ -9,11 +9,11 @@ enum { //enum to represent each type of file command
 	FILE_MOVE
 };
 
-extern char dataBaseLocation[24]; //store database location
+extern char dataBaseLocation[100]; //store database location
 extern size_t file_size; //file size variable
-extern char files[10024][24]; //variables to represent a file
-extern int fileNums[10024];
-extern char fileWithNames[10024][24];
+extern char files[5500][100]; //variables to represent a file
+extern int fileNums[5500];
+extern char fileWithNames[5500][100];
 extern int num;
 extern int totalfilenums;
 extern int totalfiles;
@@ -22,9 +22,9 @@ extern int isFile;
 extern int fileReadCount;
 extern int isFileFromCreate;
 
-extern char fileNumsForMove[10024][24]; //variables to perform query specs
+extern char fileNumsForMove[5500][100]; //variables to perform query specs
 extern int fileNumsForMoveCount;
-extern char querySpecFileName[24];
+extern char querySpecFileName[100];
 extern int splitKeyValuePairLimit;
 extern char **splitKeyPath;
 extern char **splitValuePath;
@@ -39,4 +39,3 @@ struct my_struct //struct to get key,value pairs from database
 
 extern int countglobal;
 extern struct my_struct s; //struct object
-
